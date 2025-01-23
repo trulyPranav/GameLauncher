aa4add44879241ffb89dc55fdf0836c6b638d377
Pranav M <pranavm265@gmail.com>
2025-01-24 00:36:12 +0530

    app built

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Game Launcher Excel 2024",
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
